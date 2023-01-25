34f448ea16e30c8a945abe4a72278521af006e8f
ChaiGuoliang <peter79634@stu.xjtu.edu.cn>
2023-01-25 08:16:30 +0100

    cluster run prepare

--- Setups/OpenFOAM-TwoPhaseFlow-setups/cases/hydrodynamic/oscillatingDroplet3D/run_benchmarkpaper.sh
+++ Setups/OpenFOAM-TwoPhaseFlow-setups/cases/hydrodynamic/oscillatingDroplet3D/run_benchmarkpaper.sh
@@ -3,6 +3,7 @@ mkdir study-benchmarkpaper
 create-parameter-study.py benchmarkpaper.parameter -d study-benchmarkpaper
 rm -f *.database
 cp collect_benchmark_data.sh study-benchmarkpaper
+cp interFlow.sbatch study-benchmarkpaper
 cd study-benchmarkpaper
 sh run_benchmarkpaper_study.sh
 cd ..
